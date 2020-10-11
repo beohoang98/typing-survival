55aa8468b72726e784a954807ff62da46e3d25c7
beohoang98 <beohoang98@users.noreply.github.com>
2020-10-11 15:44:30 +0700

    chore: add lighting and menu

---
+++ Assets/Scripts/Config/UserSetting.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Config
+{
+    [Serializable]
+    public class UserSetting
+    {
+        private static string _path = "user_config.json";
+
+        private int level = 1;
+        Resolution _res = Display.of["1280x720"];
+        bool _fullscreen = true;
+        
+        public void Apply()
+        {
+            Screen.SetResolution(_res.width, _res.height, _fullscreen, _res.refreshRate);
+        }
+
+        public Resolution Res
+        {
+            get => _res;
+            set
+            {
+                _res = value;
+                Apply();
+                Save();
+            }
+        }
+
+        public bool Fullscreen
+        {
+            get => _fullscreen;
+            set
+            {
+                _fullscreen = value;
+                Apply();
+                Save();
+            }
+        }
+
+        public int Level
+        {
+            get => level;
+            set
+            {
+                level = value;
+                Apply();
+                Save();
+            }
+        }
+        
+        public void Save()
+        {
+            string json = JsonUtility.ToJson(this);
+            StreamWriter writer = new StreamWriter(_path);
+            writer.WriteLine(json);
+            writer.Close();
+        }
+        
+        public static UserSetting Load()
+        {
+            UserSetting setting = Read();
+            setting.Apply();
+            return setting;
+        }
+
+        static UserSetting Read()
+        {
+            if (!File.Exists(_path))
+            {
+                var setting = new UserSetting();
+                setting.Save();
+                return setting;
+            }
+
+            try
+            {
+                StreamReader reader = new StreamReader(_path);
+                string json = reader.ReadToEnd();
+                UserSetting setting = JsonUtility.FromJson<UserSetting>(json);
+                return setting;
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.Message);
+                return new UserSetting();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Config;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Display = Config.Display;
+
+namespace UI
+{
+    public class MenuController : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button settingButton;
+        [SerializeField] private Button creditButton;
+        [SerializeField] private Button quitButton;
+        [SerializeField] private GameObject settingUI;
+        [SerializeField] private Dropdown displayDropdown;
+        [SerializeField] private Toggle fullscreenToggle;
+        [SerializeField] private Button backButton;
+
+        private UserSetting _setting;
+
+        private void Start()
+        {
+            _setting = UserSetting.Load();
+            settingUI.SetActive(false);
+            playButton.onClick.AddListener(Play);
+            settingButton.onClick.AddListener(OpenSetting);
+            // creditButton.onClick.AddListener();
+            quitButton.onClick.AddListener(Quit);
+            displayDropdown.onValueChanged.AddListener(HandleResoChange);
+            fullscreenToggle.onValueChanged.AddListener(HandleFullscreenChecked);
+            backButton.onClick.AddListener(BackMainMenu);
+        }
+
+        void Play()
+        {
+            int level = _setting.Level;
+            SceneManager.LoadScene(level);
+        }
+
+        void OpenSetting()
+        {
+            settingUI.SetActive(true);
+        }
+
+        void Quit()
+        {
+            Application.Quit();
+        }
+
+        void HandleResoChange(int idx)
+        {
+            string val = displayDropdown.options[idx].text.Replace(" ", "");
+            _setting.Res = Display.of[val];
+        }
+
+        void HandleFullscreenChecked(bool isChecked)
+        {
+            _setting.Fullscreen = isChecked;
+        }
+
+        void BackMainMenu()
+        {
+            settingUI.SetActive(false);
+        }
+    }
+}
