d631db8f1913d88450b654570de6f007db1106d5
Beo Hoang <beohoang98@users.noreply.github.com>
2021-01-19 14:02:45 +0700

    BREAKING: Refactor spawner + sound effect
    Added:
    - Fixed Position Spawner
    - Spawn sound from underground
    - Firing sound
    
    Update:
    - Code
    - Project setting

---
+++ Assets/Game/Scripts/Game/AbstractPositionSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Game
+{
+    public abstract class AbstractPositionSpawner : MonoBehaviour
+    {
+        public virtual Vector2 GetRandomPosition()
+        {
+            return Vector2.zero;
+        }
+    }
+}
